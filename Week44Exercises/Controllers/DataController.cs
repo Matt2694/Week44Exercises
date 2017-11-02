65f15dccd4d2992ab2db4f329d1da09f273d87b5
Matthew Peterson <matt2694@edu.eal.dk>
2017-11-02 15:29:57 +0100

    Add project files.

---
+++ Week44Exercises/Controllers/DataController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Week44Exercises.Models;
+
+namespace Week44Exercises.Controllers
+{
+    public class DataController : Controller
+    {
+        const string DataKey = "_Data";
+
+        [HttpGet]
+        public IActionResult Input()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Input(DataModel dataModel)
+        {
+            HttpContext.Session.Set<DataModel>(DataKey, dataModel);
+
+            return View();
+        }
+
+        public IActionResult Output()
+        {
+            DataModel data = HttpContext.Session.Get<DataModel>(DataKey);
+            ViewData["data"] = data;
+            
+            return View();
+        }
+    }
+}
---
+++ Week44Exercises/Models/DataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week44Exercises.Models
+{
+    public class DataModel
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string DOB { get; set; }
+        public string Email { get; set; }
+        public string PNumber { get; set; }
+    }
+}
